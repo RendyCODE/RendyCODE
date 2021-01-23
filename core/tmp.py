692bb2799810f98a0b428261353a5439ae656b4a
RendyCODE <77872718+RendyCODE@users.noreply.github.com>
2021-01-23 13:56:51 +0700

    Update tmp.py

--- core/tmp.py
+++ core/tmp.py
@@ -4,14 +4,18 @@ B = '\033[94m'
 Y = '\033[93m'
 W = '\033[0m'
 
-logo = f"""{B}TOOLS YANG SANGAT MEMBANTU SEKALI .... !!!{W}
-                 Tools Internet Mode Gratis{G} 1.0 unv{W}
+logo = f"""{B}
+         _/_/_/         _/_/_/_/_/                          _/
+       _/                  _/       _/_/        _/_/       _/
+      _/  _/_/  _/_/_/    _/     _/    _/    _/    _/     _/
+     _/    _/            _/     _/    _/    _/    _/     _/
+      _/_/_/            _/       _/_/        _/_/       _/ {W}
+                 Gretronger Tool Version{G} 4.0 unv{W}
 
-     By       : {Y}Status : 200 (Connection Estabilised){W}
-     Codename : {R}Rendy Arisandi{W}
-     Contact  : https://www.facebook.com/qimonx
-     Whatsapp : 083159522600
-""
+     By       : {Y}407 AUTHENTIC EXPLOIT{W}
+     Codename : {R}JaxBCD{W}
+     Contact  : https://www.facebook.com/jaka.lesmana.794629
+"""
 menu = f'''
       {G}-1-{W} Search Proxy and Check
       {G}-2-{W} Search Host
