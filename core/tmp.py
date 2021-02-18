936e4eb8097b5add75839dadf13cae09cd62cd13
RendyCODE <77872718+RendyCODE@users.noreply.github.com>
2021-02-18 20:59:45 +0700

    Update tmp.py

--- core/tmp.py
+++ core/tmp.py
@@ -4,18 +4,18 @@ B = '\033[94m'
 Y = '\033[93m'
 W = '\033[0m'
 
-logo = f"""{B} CODE {W}
-     Gretronger Tool Version{G} 4.0 unv{W}
+logo = f"""{B} BLITAR GRATONGER'S 2021 {W}
+     Gretronger Tool Version{G} 4.0 2021{W}
 
-     By       : {Y}407 AUTHENTIC EXPLOIT{W}
-     Codename : {R}JaxBCD{W}
-     Contact  : https://www.facebook.com/jaka.lesmana.794629
+     By       : {Y}Status : 200 (Connection Establiashed){W}
+     Codename : {R}BLITAR GRETONGER'S{W}
+     Contact  : https://www.facebook.com/qimonx
 """
 menu = f'''
-      {G}-1-{W} Search Proxy and Check
-      {G}-2-{W} Search Host
-      {G}-3-{W} Generate random Proxy
-      {G}-4-{W} Check Proxy in list file
-      {G}-5-{W} Check Single Proxy
-      {G}-0-{W} Exit
+      {G}1{W} Search Proxy and Check
+      {G}2{W} Search Host
+      {G}3{W} Generate random Proxy
+      {G}4{W} Check Proxy in list file
+      {G}5{W} Check Single Proxy
+      {G}0{W} Exit
         '''
